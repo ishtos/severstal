1b21bdf025643ce88c61852e5300229885db4858
ti-ginkgo <ginkgonut.9960@gmail.com>
2019-09-22 01:40:25 +0900

    update

--- py2/utils/albu_augment_util.py
+++ py2/utils/albu_augment_util.py
@@ -12,7 +12,7 @@ def albu_augment_default(image, mask=None):
 def albu_augment_normalize(image, mask=None):
     mean = (0.485, 0.456, 0.406)
     std = (0.229, 0.224, 0.225)
-    transforms = A.Compose([A.Normalize(mean, std)])
-    image = transforms(image)
+    augment = A.Normalize(mean, std)
+    image = augment(image)['image']
     
     return image, mask
