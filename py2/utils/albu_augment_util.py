eaf22a37580ac5165be5382b802fbb3f81f33733
ti-ginkgo <ginkgonut.9960@gmail.com>
2019-09-22 01:41:19 +0900

    update

--- py2/utils/albu_augment_util.py
+++ py2/utils/albu_augment_util.py
@@ -13,6 +13,6 @@ def albu_augment_normalize(image, mask=None):
     mean = (0.485, 0.456, 0.406)
     std = (0.229, 0.224, 0.225)
     augment = A.Normalize(mean, std)
-    image = augment(image)['image']
+    image = augment(image=image)['image']
     
     return image, mask
