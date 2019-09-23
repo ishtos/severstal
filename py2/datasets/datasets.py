79a2e6c2ae41c56582b8f68125aab4ca6a005215
ti-ginkgo <ginkgonut.9960@gmail.com>
2019-09-23 18:04:46 +0900

    update

--- py2/datasets/datasets.py
+++ py2/datasets/datasets.py
@@ -191,6 +191,7 @@ class SteelDataset(Dataset):
             return image, mask, index
         else:
             print(type(image))
+            print(self.transform)
             if self.transform is not None:
                 image = self.transform(image=image)
 
