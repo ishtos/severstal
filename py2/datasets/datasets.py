fde7c30f801f5e328faed6538c3ce42ad0683b3f
ti-ginkgo <ginkgonut.9960@gmail.com>
2019-09-23 19:03:11 +0900

    update

--- py2/datasets/datasets.py
+++ py2/datasets/datasets.py
@@ -193,7 +193,6 @@ class SteelDataset(Dataset):
             if self.transform is not None:
                 image = self.transform(image=image)[0]
 
-            print(type(image))
             image = image / 255.0
 
             image = image_to_tensor(image)
