03b1c51285a736378693add3ae500412c9560ace
ti-ginkgo <ginkgonut.9960@gmail.com>
2019-10-01 22:47:56 +0900

    update

--- py2/layers/loss_funcs/loss.py
+++ py2/layers/loss_funcs/loss.py
@@ -7,6 +7,22 @@ from torch.nn import BCEWithLogitsLoss, BCELoss
 from layers.loss_funcs import lovasz_losses as L
 
 
+def dice_score(prob, truth, threshold=0.5):
+    num = prob.size(0)
+
+    prob = prob > threshold
+    truth = truth > 0.5
+
+    prob = prob.view(num, -1)
+    truth = truth.view(num, -1)
+    intersection = (prob * truth)
+
+    score = 2. * (intersection.sum(1) + 1.).float() / (prob.sum(1) + truth.sum(1) + 2.).float()
+    score[score >= 1] = 1
+    score = score.sum() / num
+    return score
+
+
 class DiceLoss(nn.Module):
     def __init__(self):
         super(DiceLoss, self).__init__()
