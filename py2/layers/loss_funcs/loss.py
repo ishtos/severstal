655eb9d25886705e5f11a889a241324663ff3b00
ti-ginkgo <ginkgonut.9960@gmail.com>
2019-10-01 22:29:41 +0900

    update

--- py2/layers/loss_funcs/loss.py
+++ py2/layers/loss_funcs/loss.py
@@ -6,20 +6,38 @@ from torch.nn import BCEWithLogitsLoss, BCELoss
 
 from layers.loss_funcs import lovasz_losses as L
 
-def dice_score(prob, truth, threshold=0.5):
-    num = prob.size(0)
 
-    prob = prob > threshold
-    truth = truth > 0.5
+class DiceLoss(nn.Module):
+    def __init__(self):
+        super(DiceLoss, self).__init__()
+    
+    def forwaed(self, logits, targets, threshold):
+        num = logits.size(0)
 
-    prob = prob.view(num, -1)
-    truth = truth.view(num, -1)
-    intersection = (prob * truth)
+        logits = logits > threshold
+        targets = targets > 0.5
 
-    score = 2. * (intersection.sum(1) + 1.).float() / (prob.sum(1) + truth.sum(1) + 2.).float()
-    score[score >= 1] = 1
-    score = score.sum() / num
-    return score
+        logits = logits.view(num, -1)
+        targets = targets.view(num, -1)
+        intersection = (logits * targets)
+
+        score = 2. * (intersection.sum(1) + 1.).float() / (logits.sum(1) + targets.sum(1) + 2.).float()
+        score[score >= 1] = 1
+        score = 1 - score.sum() / num
+
+        return score
+
+
+class BCEDiceLoss(nn.Module):
+    def __init__(self):
+        super(BCEDiceLoss, self).__init__()
+        self.bce = BCELoss()
+        self.dice = DiceLoss()
+
+    def forward(self, logits, targets, threshold=0.5):
+        bce = self.bce(logits, targets)
+        dice = self.dice(logits, targets, threshold)
+        return 0.5 * bce + dice
 
 
 class SymmetricLovaszLoss(nn.Module):
@@ -28,14 +46,3 @@ class SymmetricLovaszLoss(nn.Module):
     
     def forward(self, logits, targets):
         return ((L.lovasz_hinge(logits, targets, per_image=True)) + (L.lovasz_hinge(-logits, 1-targets, per_image=True))) / 2
-
-
-class BCEDiceLoss(nn.Module):
-    def __init__(self):
-        super(BCEDiceLoss, self).__init__()
-
-    def forward(self, logits, targets, smooth=1e-5, threshold=0.5):
-        bce = F.binary_cross_entropy(logits, targets)
-        dice = dice_score(logits, targets, threshold)
-        dice = 1 - dice.sum() / num
-        return 0.5 * bce + dice
--- py2/train.py
+++ py2/train.py
@@ -25,7 +25,7 @@ from utils.augment_util import *
 from utils.albu_augment_util import *
 from utils.log_util import Logger
 
-loss_names = ['SymmetricLovaszLoss', 'BCEWithLogitsLoss', 'BCELoss']
+loss_names = ['SymmetricLovaszLoss', 'BCEWithLogitsLoss', 'BCELoss', 'BCEDiceLoss']
 split_types = ['split', 'cropv3_split', 'lung_split']
 split_names = ['random_folds4', 'random_folds10']
 
