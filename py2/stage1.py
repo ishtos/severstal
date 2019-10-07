ba2cb962cab7c8f3a7fb16cc57c43855ff60a2b2
ti-ginkgo <ginkgonut.9960@gmail.com>
2019-10-07 22:10:19 +0900

    update

--- py2/stage1.py
+++ py2/stage1.py
@@ -231,6 +231,9 @@ def train(train_loader, model, ema_model, criterion, optimizer, epoch, args, lr=
         labels = Variable(labels.cuda())
 
         outputs = model(images)
+
+        print(outputs.shape)
+        print(labels.shape)
         loss = criterion(outputs, labels)
         # loss = criterion(outputs, masks, epoch=epoch)
 
